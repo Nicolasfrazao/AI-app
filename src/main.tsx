d49dc8881f1c3fa6d5ab4494a83e908d2225f2a4
Nick Frazao <nicolas_frazao@hotmail.com>
2024-06-25 16:06:34 -0300

    Final version of front end app with vite and react

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import App from './App.tsx'
+import './styles/index.css'
+
+const root : any = document.getElementById( 'root' );
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+)
